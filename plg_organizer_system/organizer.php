d3243f28c7588faf604ec5ef54c6577880f23ec7
James Antrim <james.antrim@nm.thm.de>
2020-06-16 13:37:34 +0200

    Corrects language constants. Autoformatted.

--- plg_organizer_system/organizer.php
+++ plg_organizer_system/organizer.php
@@ -25,34 +25,35 @@ defined('_JEXEC') or die;
  */
 class PlgSystemOrganizer extends JPlugin
 {
-    /**
-     * Adds additional fields to the user editing form
-     *
-     * @param   Form   $form  The form to be altered.
-     * @param   mixed  $data  The associated data for the form.
-     *
-     * @return  boolean
-     */
-    public function onContentPrepareForm(Form $form, $data)
-    {
-        // Check we are manipulating a valid form.
-        $name = $form->getName();
+	/**
+	 * Adds additional fields to the user editing form
+	 *
+	 * @param   Form   $form  The form to be altered.
+	 * @param   mixed  $data  The associated data for the form.
+	 *
+	 * @return  boolean
+	 */
+	public function onContentPrepareForm(Form $form, $data)
+	{
+		// Check we are manipulating a valid form.
+		$name = $form->getName();
 
-        if ($name !== 'com_menus.item' or !is_object($data) or empty($data->request))
-        {
-            return true;
-        }
+		if ($name !== 'com_menus.item' or !is_object($data) or empty($data->request))
+		{
+			return true;
+		}
 
-        $option = empty($data->request['option']) ? '' : $data->request['option'];
-        $view = empty($data->request['view']) ? '' : $data->request['view'];
-        if (empty($option) or $option !== 'com_thm_organizer' or empty($view)) {
-            return true;
-        }
+		$option = empty($data->request['option']) ? '' : $data->request['option'];
+		$view   = empty($data->request['view']) ? '' : $data->request['view'];
+		if (empty($option) or $option !== 'com_thm_organizer' or empty($view))
+		{
+			return true;
+		}
 
-        // Add the view configuration fields to the form.
-        FormHelper::addFormPath(JPATH_ROOT . '/components/com_organizer/Layouts/HTML');
-        $form->loadFile($view);
+		// Add the view configuration fields to the form.
+		FormHelper::addFormPath(JPATH_ROOT . '/components/com_organizer/Layouts/HTML');
+		$form->loadFile($view);
 
-        return true;
-    }
+		return true;
+	}
 }
